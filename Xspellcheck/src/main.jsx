a905d600068937e5d706adbf302bef7fcb5473e0
Syed Faheem <syedfaheem2719@gmail.com>
2024-03-27 23:41:00 +0530

    Add Xspell check

---
+++ Xspellcheck/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SpellCheck from './SpellCheckApp.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SpellCheck />
+  </React.StrictMode>,
+)
